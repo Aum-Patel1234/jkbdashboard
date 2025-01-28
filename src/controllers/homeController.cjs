791b17f4b67b943d79dd66b1101b4c583dfeb773
Aum Patel <aumpatel131@gmail.com>
2025-01-28 17:05:44 +0530

    ready to start CRUD..

--- src/controllers/homeController.cjs
+++ src/controllers/homeController.cjs
@@ -1,3 +1,25 @@
 exports.getAllUsers = (req, res) => {
-  res.send('all Users');
+  // console.log(req.path);
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/users'});
+};
+exports.getAllSubjects = (req, res) => {
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/subjects' });
+};
+exports.getAllPackages = (req, res) => {
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/packages' });
+};
+exports.getAllBranches = (req, res) => {
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/branches' });
+};
+exports.getAllPayments = (req, res) => {
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/payments' });
+};
+exports.predict = (req, res) => {
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/predict' });
+};
+exports.attendance = (req, res) => {
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/attendance' });
+};
+exports.tests = (req, res) => {
+  res.render('layout.ejs', { currentPath: req.path , contentPath: 'partials/tests' });
 };
--- src/routes/routes.cjs
+++ src/routes/routes.cjs
@@ -3,9 +3,20 @@ const router = express.Router();
 const controller = require('../controllers/homeController.cjs');
 
 router.get('/', (req, res) => {
-  res.render('index.ejs');
+  res.redirect('/users');
 });
 
 router.get('/users', controller.getAllUsers);
+router.get('/subjects', controller.getAllSubjects);
+router.get('/packages', controller.getAllPackages);
+router.get('/branches', controller.getAllBranches);
+router.get('/payments', controller.getAllPayments);
+router.get('/predict', controller.predict);
+router.get('/attendance', controller.attendance);
+router.get('/tests', controller.tests);
+
+router.get('/logout', (req, res)=>{
+  res.redirect('/users');
+});
 
 module.exports = router;
